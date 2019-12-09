c6fceb6c7a132b35fc67435fa21688ae4d93be6f
al7262 <alta5@alta5>
2019-12-09 10:38:22 +0700

    DJANGO first step

---
+++ proj_adv_views/env_proj_adv_views/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/alta5/Documents/DJANGO/DJANGO_MVC/proj_adv_views/env_proj_adv_views/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
